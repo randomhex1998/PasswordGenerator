fe5137d6b45e70dc40ea3c0aadcc8a03247887ab
Rayan <randomhex1998@gmail.com>
2022-09-13 18:42:55 +0430

    import-html-elements-in-js-file

---
+++ js/main.js
@@ -0,0 +1,3 @@
+let inputBoxShow = document.querySelector("#show-input-box");
+let generateBtn = document.getElementsByClassName("gen")[0];
+let copyBtn = document.getElementsByClassName("copy")[0];
